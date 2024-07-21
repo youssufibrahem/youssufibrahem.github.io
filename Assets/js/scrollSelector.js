773a01bc80f08daf1833a2a7f0f1df829226452a
amer266030 <amer266030@users.noreply.github.com>
2024-07-21 10:42:58 +0300

    updates 1

--- Assets/js/scrollSelector.js
+++ Assets/js/scrollSelector.js
@@ -3,6 +3,7 @@ document.querySelectorAll('.podcast-scroll-content').forEach(item => {
     item.addEventListener('click', function() {
         const videoUrl = this.getAttribute('data-video-url') || this.getAttribute('video-url');
         const videoTitle = this.getAttribute('data-video-title') || this.getAttribute('video-title');
+        const videoLanguage = this.getAttribute('lang');
         const videoPlayer = document.getElementById('podcast-video-player');
         const videoTitleElement = document.getElementById('podcast-video-title');
 
@@ -14,6 +15,13 @@ document.querySelectorAll('.podcast-scroll-content').forEach(item => {
 
         // Update the video title
         videoTitleElement.textContent = videoTitle;
+
+        if (videoLanguage === 'ar') {
+            videoTitleElement.style.fontFamily = 'Noto Kufi Arabic';
+        } else {
+            videoTitleElement.style.fontFamily = 'Source Sans Pro';
+        }
+
     });
 });
 
